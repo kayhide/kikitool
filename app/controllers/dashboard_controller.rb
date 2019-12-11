1392cb089f1be7346f7ee7c644778b8ee54a63d7
Hideaki Kawai <kayhide@gmail.com>
2019-12-11 18:42:46 +0900

    Update dashboard view

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,8 +2,7 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @audios =
-      current_user.audios_blobs.order(id: :desc)
-        .each.with_object(Audio).map(&:becomes)
+    @transcriptions =
+      current_user.transcriptions.order(id: :desc)
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -41,7 +41,7 @@ module Kikitool
     end
 
     config.i18n.available_locales = [:en, :ja]
-    config.i18n.default_locale = :ja
+    config.i18n.default_locale = :en
     config.time_zone = "Tokyo"
   end
 end
