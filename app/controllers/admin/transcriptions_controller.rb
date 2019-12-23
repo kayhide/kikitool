0564dadedb5507e27771333ada80bf6b32fb46f3
Hideaki Kawai <kayhide@gmail.com>
2019-12-23 14:03:38 +0900

    Order transcriptions on admin dashboard

--- app/controllers/admin/transcriptions_controller.rb
+++ app/controllers/admin/transcriptions_controller.rb
@@ -1,7 +1,9 @@
 module Admin
   class TranscriptionsController < Admin::ApplicationController
     def scoped_resource
-      resource_class.with_attached_audio
+      resource_class
+        .order(id: :desc)
+        .with_attached_audio
     end
     # Overwrite any of the RESTful controller actions to implement custom behavior
     # For example, you may want to send an email after a foo is updated.
